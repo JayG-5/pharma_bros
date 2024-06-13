bf20123cfe172cf8a9e5a4045af9a7c5f32abab4
JayG-5 <jg55552758@gmail.com>
2024-06-13 19:27:58 +0900

    chore: 자료형기입

--- lib/styles/typography.dart
+++ lib/styles/typography.dart
@@ -2,40 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:pharmabros/styles/style.dart';
 
 class AppTypography{
-  get heading2 => TextStyle(
+  TextStyle get heading2 => TextStyle(
     fontWeight: FontWeight.w700,
     color: AppStyle.color.primaryText,
     fontSize: 20,
     height: 1.5,
     letterSpacing: -0.03,
   );
-  get heading3 => TextStyle(
+  TextStyle get heading3 => TextStyle(
     fontWeight: FontWeight.w700,
     color: AppStyle.color.primaryText,
     fontSize: 16,
     height: 1.5,
     letterSpacing: -0.03,
   );
-  get heading4 => TextStyle(
+  TextStyle get heading4 => TextStyle(
     fontWeight: FontWeight.w700,
     color: AppStyle.color.primaryText,
     fontSize: 14,
     height: 1.5,
     letterSpacing: -0.03,
   );
-  get body1 => TextStyle(
+  TextStyle get body1 => TextStyle(
     fontWeight: FontWeight.w400,
     color: AppStyle.color.primaryText,
     fontSize: 16,
     height: 1.5,
   );
-  get body2 => TextStyle(
+  TextStyle get body2 => TextStyle(
     fontWeight: FontWeight.w400,
     color: AppStyle.color.hintText,
     fontSize: 14,
     height: 1.5,
   );
-  get body3 => const TextStyle(
+  TextStyle get body3 => const TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 12,
     height: 1.5,
