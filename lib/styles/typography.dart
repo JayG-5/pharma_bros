c2ef001f01e9469ef0261035ab1c0d1aef314694
JayG-5 <jg55552758@gmail.com>
2024-06-13 18:52:56 +0900

    fix: textStyle height 수정

--- lib/styles/typography.dart
+++ lib/styles/typography.dart
@@ -6,38 +6,38 @@ class AppTypography{
     fontWeight: FontWeight.w700,
     color: AppStyle.color.primaryText,
     fontSize: 20,
-    height: 30,
+    height: 1.5,
     letterSpacing: -0.03,
   );
   get heading3 => TextStyle(
     fontWeight: FontWeight.w700,
     color: AppStyle.color.primaryText,
     fontSize: 16,
-    height: 24,
+    height: 1.5,
     letterSpacing: -0.03,
   );
   get heading4 => TextStyle(
     fontWeight: FontWeight.w700,
     color: AppStyle.color.primaryText,
     fontSize: 14,
-    height: 21,
+    height: 1.5,
     letterSpacing: -0.03,
   );
   get body1 => TextStyle(
     fontWeight: FontWeight.w400,
     color: AppStyle.color.primaryText,
     fontSize: 16,
-    height: 24,
+    height: 1.5,
   );
   get body2 => TextStyle(
     fontWeight: FontWeight.w400,
     color: AppStyle.color.hintText,
     fontSize: 14,
-    height: 21,
+    height: 1.5,
   );
-  get body3 => TextStyle(
+  get body3 => const TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 12,
-    height: 18,
+    height: 1.5,
   );
 }
