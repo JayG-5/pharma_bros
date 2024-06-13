5f294b6d31ad4b4b5a6013112d06b739163350ec
JayG-5 <jg55552758@gmail.com>
2024-06-13 17:21:19 +0900

    style: typography, color

---
+++ lib/styles/style.dart
@@ -0,0 +1,8 @@
+import 'package:pharmabros/styles/color.dart';
+import 'package:pharmabros/styles/typography.dart';
+
+class AppStyle{
+
+  static final color = AppColor();
+  static final typography = AppTypography();
+}
